3a08cb1c1b590feaa8b2eb503a5601e68f58f032
zhaojianping <86882722@qq.com>
2017-02-21 14:15:50 +0800

    测试通过

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,11 @@
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+//@SpringBootApplication
 @RestController
+@EnableAutoConfiguration
 public class Application {
 
     @RequestMapping("/")
