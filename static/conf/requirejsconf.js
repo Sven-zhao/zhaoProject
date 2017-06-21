f9021e4a4040ea9224cbc990e1a7b876928f6ade
zhaojianping <zhaojianping@im-local-server.(none)>
2017-06-21 14:14:09 +0800

    del zjp

--- static/conf/requirejsconf.js
+++
@@ -1,8 +0,0 @@
-({
-	baseUrl: '/Users/wentao/Work/newwmp/wmp-webapp/src/main/webapp/static/wmp',
-	paths: {
-		common: 'common',
-		modules: 'modules',
-		jquery: 'common/jquery/jquery-2.1.4.min'
-	}
-})
